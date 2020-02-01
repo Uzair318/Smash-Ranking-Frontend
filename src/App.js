5f46cd177258bf50b0c9f831f37f4d158ad91c97
Uzair <Uzair318@vt.edu>
2020-01-31 20:51:54 -0500

    created loginform class and added it to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Table from './table';
+import LoginForm from './LoginForm';
 
 
 function App() {
   return (
     <div className="App">
+      <LoginForm />
+
       <Table />
     </div>
   );
